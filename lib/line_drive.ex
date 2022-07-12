0deee0b9d97fcf5355221c17d6e2f2104d5386ef
Tim Mecklem <timothy@mecklem.com>
2022-07-12 16:22:25 -0400

    Delegate the api commands as a facade at the LineDrive module level

--- lib/line_drive.ex
+++ lib/line_drive.ex
@@ -5,6 +5,10 @@ defmodule LineDrive do
 
   use Tesla
 
+  defdelegate get_deal(client, deal_id), to: LineDrive.Deals
+  defdelegate list_pipelines(client), to: LineDrive.Pipelines
+  defdelegate list_pipeline_deals(client, pipeline_id), to: LineDrive.Pipelines
+
   def client(api_token, base_url) do
     middleware = [
       {Tesla.Middleware.BaseUrl, base_url},
