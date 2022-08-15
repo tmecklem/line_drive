f1991235c1a9177cce01c2eed9fab2381514a093
Tim Mecklem <timothy@mecklem.com>
2022-08-15 10:00:08 -0400

    Version bump to 0.9.0

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule LineDrive.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "0.8.0"
+      version: "0.9.0"
     ]
   end
 
