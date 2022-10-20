13de1e314a787e3e466accfd19f342f63f2a58ce
Al White <al@launchscout.com>
2022-10-20 17:21:20 -0400

    Use organization struct

--- lib/line_drive/person.ex
+++ lib/line_drive/person.ex
@@ -4,14 +4,14 @@ defmodule LineDrive.Person do
   """
 
   use TypedStruct
-  import LineDrive, only: [flatten: 1]
 
   typedstruct do
     field :id, pos_integer()
     field :name, String.t(), enforce: true
     field :owner_id, pos_integer()
     field :primary_email, String.t()
-    field :organization_name, String.t()
+    # search returns an organization map
+    field :organization, LineDrive.Organization.t()
   end
 
   defimpl Jason.Encoder, for: __MODULE__ do
@@ -23,6 +23,13 @@ defmodule LineDrive.Person do
   end
 
   def new(map) do
-    struct(__MODULE__, flatten(map))
+    org =
+      map
+      |> Map.get(:organization, %{})
+      |> Kernel.||(%{})
+      |> LineDrive.Organization.new()
+
+    map = Map.put(map, :organization, org)
+    struct(__MODULE__, map)
   end
 end
