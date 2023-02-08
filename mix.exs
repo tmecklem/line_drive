1072dd35e4f6673bfbd5e3d9f1bb6d0519b47903
s-loar <sjl7909@gmail.com>
2023-02-08 13:07:35 -0500

    Bump mix to v0.16.0

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule LineDrive.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "0.15.0"
+      version: "0.16.0"
     ]
   end
 
