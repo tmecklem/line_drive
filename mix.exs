b557801ba04cdfa409af118c0709bbbe2eaf4fc3
Tim Mecklem <timothy@mecklem.com>
2025-02-10 23:20:09 -0500

    Bump version to 0.27.0

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule LineDrive.MixProject do
       elixirc_options: [warnings_as_errors: true],
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "0.26.0"
+      version: "0.27.0"
     ]
   end
 
